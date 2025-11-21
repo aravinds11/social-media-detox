09805af9ee599f274acb12109cd72ad3fed4b7f6
Aravind S <174857986+aravinds11@users.noreply.github.com>
2025-11-21 11:42:08 +0530

    Add axios interceptor to include JWT token in all API requests

--- mobile/src/api/api.js
+++ mobile/src/api/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import Constants from "expo-constants";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const { apiUrl } = Constants.expoConfig.extra;
 
@@ -7,3 +8,14 @@ export const api = axios.create({
   baseURL: apiUrl,
   headers: { "Content-Type": "application/json" },
 });
+
+api.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => Promise.reject(err)
+);
